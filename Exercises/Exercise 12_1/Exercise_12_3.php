8ff456c12c5ef5ea1e2fb11879dcf3e2017b4a02
MustafaKH96 <74243634+MustafaKh96@users.noreply.github.com>
2022-01-30 21:37:02 +0100

    ...

--- Exercises/Exercise 12_1/Exercise_12_3.php
+++ Exercises/Exercise 12_1/Exercise_12_3.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Einführung</title>
+    <title>PHP</title>
     <link rel="stylesheet" href="../Exercise 1/Exrcise_1.css">
     <style>
         table{
